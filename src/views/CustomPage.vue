4f0bffb268da8b6e9c8c9099d3f02afa61dc4b39
long2ice <long2ice@gmail.com>
2020-08-12 10:49:19 +0800

    update CustomPage.vue

--- src/views/CustomPage.vue
+++ src/views/CustomPage.vue
@@ -1,62 +1,29 @@
 <template>
-  <div>
-    <custom-component :config="page"></custom-component>
-  </div>
+  <div v-if="data.html" v-html="data.html"></div>
 </template>
-<style>
-.nopadding {
-  padding: 0 !important;
-}
-</style>
+
 <script>
 export default {
-  props: {},
   data() {
     return {
-      loaded: false,
-      name: null,
-      page: null
+      data: {},
+      html: ""
     };
   },
-
   computed: {
     uri() {
       return this.$route.params.uri.replace(/\./g, "/");
     }
   },
-  watch: {
-    $route: "fetch"
-  },
   methods: {
     fetch() {
-      this.fetchPage();
-    },
-    render() {},
-    fetchPage() {
-      this.$http.get(this.uri).then(({ data }) => {
-        data.name = "server-page-" + new Date().getTime().toString();
-        this.page = Object.assign({}, data);
+      this.$http.get(this.uri).then(({data}) => {
+        this.data = data;
       });
-    },
-
-    onSuccess(data) {
-      const { message, then, redirect } = data;
-      if (message) {
-        this.$snotify.success(message);
-      }
-      if (then) {
-        eval(then);
-      } else if (redirect) {
-        this.$router.push({ path: redirect });
-      } else {
-        this.$router.go(-1);
-      }
     }
   },
-  mounted() {},
   created() {
-    this.fetchPage();
+    this.fetch();
   }
 };
 </script>
-
